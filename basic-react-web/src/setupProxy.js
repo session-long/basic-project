a9d5d7604b2d1c9f6e26a0f575f23b27780b5ea5
CD048 <wangshengke@cetcbigdata.com>
2020-06-02 14:16:51 +0800

    由于antd@3.X使用的http-proxy-middleware@0.X

--- basic-react-web/src/setupProxy.js
+++ basic-react-web/src/setupProxy.js
@@ -1,6 +1,6 @@
-const { createProxyMiddleware } = require('http-proxy-middleware');
+const proxy = require('http-proxy-middleware');
 module.exports = function (app) {
-    app.use(createProxyMiddleware('/api', {
+    app.use(proxy('/api', {
         target: 'http://192.168.1.249:9501',
         // target: 'http://192.168.1.20:9501',
         changeOrigin: true,
