31c1c915da9927605d0959e127a1f456fccee4a8
CD048 <wangshengke@cetcbigdata.com>
2020-06-04 09:24:40 +0800

    增加antd的适配

--- basic-react-web/src/index.js
+++ basic-react-web/src/index.js
@@ -1,8 +1,16 @@
-import './index.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+
+// 由于 antd 组件的默认文案是英文，所以需要修改为中文
+import zhCN from 'antd/es/locale/zh_CN';
+import moment from 'moment';
+import 'moment/locale/zh-cn';
+import 'antd/dist/antd.css';
+import './index.css';
+
+moment.locale('zh-cn');
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
